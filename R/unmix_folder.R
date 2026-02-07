2729244d60e3837968343eec1993a268f4ca438c
DrCytometer <olivertburton@gmail.com>
2026-02-07 18:07:53 +0000

    More checks, update argument defaults

--- R/unmix_folder.R
+++ R/unmix_folder.R
@@ -9,19 +9,24 @@
 #'
 #' @importFrom lifecycle deprecate_warn
 #'
-#' @param fcs.dir Directory containing FCS files to be unmixed.
-#' @param spectra Matrix containing spectra information.
+#' @param fcs.dir Directory (file path) containing FCS files to be unmixed.
+#' @param spectra A matrix containing the spectral data. Fluorophores in rows,
+#' detectors in columns.
 #' @param asp The AutoSpectral parameter list.
 #' Prepare using `get.autospectral.param`
 #' @param flow.control A list containing flow cytometry control parameters.
-#' @param method A character string specifying the unmixing method to use.
-#' The default is `Automatic`, which uses `AutoSpectral` for AF extraction if
-#' af.spectra are provided and automatically selects `OLS` or `WLS` depending
-#' on which is normal for the given cytometer in `asp$cytometer`. This means
-#' that files from the ID7000, A8 and S8 will be unmixed using `WLS` while
-#' others will be unmixed with `OLS`. Any option can be set manually.
-#' Manual options are `OLS`, `WLS`, `AutoSpectral`, `Poisson` and `FastPoisson`.
-#' Default is `OLS`. `FastPoisson` requires installation of `AutoSpectralRcpp`.
+#' @param method A character string specifying the unmixing method to use. The
+#' default as of version 1.0.0 is now `AutoSpectral` to avoid confusion. To use
+#' AutoSpectral unmixing, you must provide at least `af.spectra` to perform
+#' autofluorescence extraction (on a per-cell basis). To also optimize
+#' fluorophore spectra, provide `spectra.variants`. To perform other types of
+#' unmixing, select from the options: `OLS`, `WLS`, `Poisson` or `FastPoisson`.
+#' `FastPoisson` requires installation of `AutoSpectralRcpp`.There is also
+#' `Automatic`, which switches depending on the inputs provided: it uses
+#' `AutoSpectral` for AF extraction if af.spectra are provided, and automatically
+#' selects `OLS` or `WLS` depending on which is normal for the given cytometer
+#' in `asp$cytometer`. This means that files from the ID7000, A8 and S8 will be
+#' unmixed using `WLS` while others will be unmixed with `OLS`.
 #' @param weighted Logical, whether to use ordinary or weighted least squares
 #' unmixing as the base algorithm in AutoSpectral unmixing.
 #' Default is `FALSE` and will use OLS.
@@ -38,44 +43,50 @@
 #' `get.spectral.variants`. Default is `NULL`. Used for
 #' AutoSpectral unmixing. Required for per-cell fluorophore optimization.
 #' @param output.dir Directory to save the unmixed FCS files
-#' (default is asp$unmixed.fcs.dir).
+#' (default is `asp$unmixed.fcs.dir`, which is `./AutoSpectral_unmixed`).
 #' @param file.suffix A character string to append to the output file name.
 #' Default is `NULL`
-#' @param include.raw Logical indicating whether to include raw data in the
-#' written FCS file. Default is `FALSE`
-#' @param include.imaging Logical indicating whether to include imaging data in
-#' the written FCS file: relevant for S8 and A8. Default is `FALSE`
+#' @param include.raw A logical value indicating whether to include raw
+#' expression data in the written FCS file. Default is `FALSE` to provide smaller
+#' output files.
+#' @param include.imaging A logical value indicating whether to include imaging
+#' parameters in the written FCS file. Default is `FALSE` to provide smaller
+#' output files.
 #' @param use.dist0 Logical, controls whether the selection of the optimal AF
 #' signature for each cell is determined by which unmixing brings the fluorophore
 #' signals closest to 0 (`use.dist0` = `TRUE`) or by which unmixing minimizes the
 #' per-cell residual (`use.dist0` = `FALSE`). Default is `TRUE`. Used for
-#' AutoSpectral unmixing.
-#' @param divergence.threshold Numeric. Used for `FastPoisson` only. Threshold
-#' to trigger reversion towards WLS unmixing when Poisson result diverges.
-#' Default is `1e4`
+#' AutoSpectral unmixing. The minimization of fluorophore signals can be thought
+#' of as a "worst-case" scenario, but it provides more accurate assignments,
+#' particularly with large panels.
+#' @param divergence.threshold Numeric. Used for `FastPoisson` only.
+#' Threshold to trigger reversion towards WLS unmixing when Poisson result
+#' diverges for a given point. To be deprecated.
 #' @param divergence.handling String. How to handle divergent cells from Poisson
-#' IRLS. Options are `NonNeg`, in which case non-negativity will be enforced,
-#' `WLS`, where values will revert to the WLS initial unmix or `Balance`,
-#' where `WLS` and `NonNeg` results will be averaged. Default is `Balance`
+#' IRLS. Options are `NonNeg` (non-negativity will be enforced), `WLS` (revert
+#' to WLS initial unmix) or `Balance` (WLS and NonNeg will be averaged).
+#' Default is `Balance`. To be deprecated.
 #' @param balance.weight Numeric. Weighting to average non-convergent cells.
-#' Used for `Balance` option under `divergence.handling`. Default is `0.5`
+#' Used for `Balance` option under `divergence.handling`. Default is `0.5`.
+#' To be deprecated.
 #' @param speed Selector for the precision-speed trade-off in AutoSpectral per-cell
-#' fluorophore optimization. Options are the default `fast`, which selects the
-#' best spectral fit per cell by updating the predicted values for each
-#' fluorophore independently without repeating the unnmixing, `medium` which uses
-#' a Woodbury-Sherman-Morrison rank-one updating of the unnmixing matrix for
-#' better results and a moderate slow-down, or `slow`, which explicitly
-#' recomputes the unmixing matrix for each variant for maximum precision. The
-#' `fast` method is only one available in the `AutoSpectral` package and will be
-#' slow in the pure R implementation. Installation of `AutoSpectralRcpp` is
-#' strongly encouraged.
+#' fluorophore optimization. Options are `fast`, `medium` and `slow`, with the
+#' default being `slow`. As of version 1.0.0, the backend for how this works
+#' has changed. Spectral variants and AF signatures are now pre-screened per cell
+#' to identify likely candidates, so brute force testing of all variants is no
+#' longer required. So, `speed` controls the number of variants to be tested per
+#' cell, with `fast` testing a single variant, `medium` testing 3 variants, and
+#' `slow` testing 10 variants. While this is now implemented in pure R in
+#' `AutoSpectral`, installation of `AutoSpectralRcpp` is strongly encouraged for
+#' faster processing.
 #' @param parallel Logical, default is `FALSE`. Set to `TRUE` to activate parallel
 #' processing for multiple FCS files.
 #' @param threads Numeric, default is `NULL`, in which case `asp$worker.process.n`
 #' will be used. `asp$worker.process.n` is set by default to be one less than the
 #' available cores on the machine. Multi-threading is only used if `parallel` is
-#' `TRUE`.
-#' @param verbose Logical, controls messaging. Default is `TRUE`.
+#' `TRUE`. If working on a computing cluster, try `parallelly::availableCores()`.
+#' @param verbose Logical, controls messaging. Default is `TRUE`. Set to `FALSE`
+#' to have it shut up.
 #' @param ... Ignored. Previously used for deprecated arguments such as
 #' `calculate.error`.
 #'
@@ -88,7 +99,7 @@ unmix.folder <- function(
     spectra,
     asp,
     flow.control,
-    method = "Automatic",
+    method = "AutoSpectral",
     weighted = FALSE,
     weights = NULL,
     af.spectra = NULL,
@@ -101,7 +112,7 @@ unmix.folder <- function(
     divergence.threshold = 1e4,
     divergence.handling = "Balance",
     balance.weight = 0.5,
-    speed = "fast",
+    speed = "slow",
     parallel = FALSE,
     threads = NULL,
     verbose = TRUE,
@@ -117,6 +128,51 @@ unmix.folder <- function(
       "unmix.folder(calculate.error)",
       "no longer used"
     )
+  }
+
+  # include checks on inputs if AutoSpectral unmixing has been selected
+  if ( method == "AutoSpectral" ) {
+    # check for af.spectra, stop if not
+    if ( is.null( af.spectra ) ) {
+      stop(
+        "For AutoSpectral unmixing, `af.spectra` must be provided.
+        See `?get.af.spectra()`.",
+        call. = FALSE
+      )
+    }
+    # check that af.spectra is a matrix and has rows
+    if ( !is.matrix( af.spectra ) || nrow( af.spectra ) < 2 ) {
+      stop(
+        "For AutoSpectral unmixing, multiple AF in `af.spectra` must be provided
+        in a matrix. See `?get.af.spectra`.",
+        call. = FALSE
+      )
+    }
+    # check for variants, warn if not provided
+    if ( is.null( spectra.variants ) ) {
+      warning(
+        "For AutoSpectral unmixing, providing fluorophore variation with `spectra.variants`
+        will give better results. See `?get.spectral.variants`.",
+        call. = FALSE
+      )
+    }
+    # check for AutoSpectralRcpp
+    if ( requireNamespace( "AutoSpectralRcpp", quietly = TRUE ) ) {
+      # require 1.0.0 or higher if installed for compatibility
+      if ( utils::packageVersion( "AutoSpectralRcpp" ) < package_version( "1.0.0" ) ) {
+        stop(
+          "Package `AutoSpectralRcpp` >= 1.0.0 is required for this method.
+          Please update the package.",
+          call. = FALSE
+        )
+      }
+    } else {
+      # warn if not available (default to R processing)
+      warning(
+        "Package `AutoSpectralRcpp` not found. Please install it for faster processing.",
+        call. = FALSE
+      )
+    }
   }
 
   # set up, create output folders where FCS files will go
@@ -173,7 +229,7 @@ unmix.folder <- function(
   }
 
   # unmix all files in list
-  unmixed.data <- tryCatch( {
+  tryCatch( {
     lapply.function( files.to.unmix, function( f ) {
       do.call( unmix.fcs, c( list( f ), args.list ) )
     } )
